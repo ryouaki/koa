d3569ccf27f91262cc787b06469b14ad00e0a542
LiangYaHui <46517115@qq.com>
2022-08-26 14:43:46 +0800

    v1.3.6 performance optimize 20% plus

--- example/main.go
+++ example/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"github.com/go-redis/redis"
+	"encoding/json"
+	"fmt"
+
 	"github.com/ryouaki/koa"
-	"github.com/ryouaki/koa/session"
+	"github.com/ryouaki/koa/static"
 )
 
 // func init() {
@@ -29,94 +31,97 @@ import (
 func main() {
 	app := koa.New()
 
-	rds := redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs: []string{"42.192.194.38:6001"},
-	})
-	store := session.NewRedisStore(rds)
-	app.Use(session.Session(session.SessionConf{
-		MaxAge: 1000,
-	}, store))
+	// rds := redis.NewUniversalClient(&redis.UniversalOptions{
+	// 	Addrs: []string{"42.192.194.38:6001"},
+	// })
+	// store := session.NewRedisStore(rds)
+	// app.Use(session.Session(session.SessionConf{
+	// 	MaxAge: 1000,
+	// }, store))
 
-	app.Get("/", func(ctx *koa.Context, next koa.Next) {
-		sess := ctx.GetData("session")
-		_sess := sess.(map[string]interface{})
-		_sess["data"] = "hello world"
-		ctx.SetData("session", _sess)
+	// app.Get("/", func(ctx *koa.Context, next koa.Next) {
+	// 	sess := ctx.GetData("session")
+	// 	_sess := sess.(map[string]interface{})
+	// 	_sess["data"] = "hello world"
+	// 	ctx.SetData("session", _sess)
+	// })
+	app.Get("/ping/:aaa", func(ctx *koa.Context, next koa.Next) {
+		ctx.SetBody([]byte("Hello World"))
 	})
+
+	// log.New(&log.Config{
+	// 	Level:   log.LevelInfo,
+	// 	Mode:    log.LogFile,
+	// 	MaxDays: 1,
+	// 	LogPath: "./../logs",
+	// })
+	// app.Use(plugin.Duration)
+	// rds := redis.NewUniversalClient(&redis.UniversalOptions{
+	// 	Addrs: []string{"42.192.194.38:6001"},
+	// })
+
+	// // store := session.NewRedisStore(rds)
+	// // app.Use(session.Session(&session.Config{
+	// // 	Store:  store,
+	// // 	MaxAge: 100,
+	// // }))
+
+	// // store := session.NewMemStore()
+	// // app.Use(session.Session(&session.Config{
+	// // 	Store:  store,
+	// // 	MaxAge: 1000,
+	// // }))
+	app.Use(static.Static("./static", "/static/"))
+	app.Use(func(ctx *koa.Context, next koa.Next) {
+		fmt.Println("b-use1")
+		next()
+		fmt.Println("b-us2e")
+	})
+	app.Use("/a", func(ctx *koa.Context, next koa.Next) {
+		fmt.Println("a")
+		next()
+	})
+	app.Get("/b", func(ctx *koa.Context, next koa.Next) {
+		fmt.Println("b1")
+		ctx.SetBody([]byte("Hello world"))
+		fmt.Println("b2")
+	})
+	app.Get("/c", func(ctx *koa.Context, next koa.Next) {
+		ctx.Status = 500
+		fmt.Println("c")
+		// next(nil)
+	})
+
+	app.Get("/d", func(ctx *koa.Context, next koa.Next) {
+		fmt.Println("d1")
+		next()
+	}, func(ctx *koa.Context, next koa.Next) {
+		fmt.Println("d2")
+		next()
+	})
+
+	app.Get("/e/:f", func(ctx *koa.Context, next koa.Next) {
+		fmt.Println("c", ctx.Params)
+		// next(nil)
+	})
+
+	app.Get("/json", func(ctx *koa.Context, next koa.Next) {
+		ctx.SetHeader("Content-Type", "application/json")
+		data := make(map[string]interface{})
+		data["test"] = "test"
+		ret, _ := json.Marshal(data)
+		ctx.SetBody(ret)
+	})
+
+	app.Use(func(ctx *koa.Context, next koa.Next) {
+		ctx.Status = 404
+		ctx.SetBody([]byte("Request not found"))
+	})
+	// err := app.Run(8080)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// }
 
 	app.Run(8080)
 }
-
-// log.New(&log.Config{
-// 	Level:   log.LevelInfo,
-// 	Mode:    log.LogFile,
-// 	MaxDays: 1,
-// 	LogPath: "./../logs",
-// })
-// app.Use(plugin.Duration)
-// rds := redis.NewUniversalClient(&redis.UniversalOptions{
-// 	Addrs: []string{"42.192.194.38:6001"},
-// })
-
-// // store := session.NewRedisStore(rds)
-// // app.Use(session.Session(&session.Config{
-// // 	Store:  store,
-// // 	MaxAge: 100,
-// // }))
-
-// // store := session.NewMemStore()
-// // app.Use(session.Session(&session.Config{
-// // 	Store:  store,
-// // 	MaxAge: 1000,
-// // }))
-// app.Use(static.Static("./static", "/static/"))
-// app.Use(func(err error, ctx *koa.Context, next koa.Next) {
-// 	fmt.Println("b-use1")
-// 	next(nil)
-// 	fmt.Println("b-us2e")
-// })
-// app.Use("/a", func(err error, ctx *koa.Context, next koa.Next) {
-// 	fmt.Println("a")
-// 	next(nil)
-// })
-// app.Get("/b", func(err error, ctx *koa.Context, next koa.Next) {
-// 	fmt.Println("b1")
-// 	ctx.SetBody([]byte("Hello world"))
-// 	fmt.Println("b2")
-// })
-// app.Get("/c", func(err error, ctx *koa.Context, next koa.Next) {
-// 	ctx.Status = 500
-// 	fmt.Println("c")
-// 	// next(nil)
-// })
-
-// app.Get("/d", func(err error, ctx *koa.Context, next koa.Next) {
-// 	fmt.Println("d1")
-// 	next(nil)
-// }, func(err error, ctx *koa.Context, next koa.Next) {
-// 	fmt.Println("d2")
-// 	next(nil)
-// })
-
-// app.Get("/e/:f", func(err error, ctx *koa.Context, next koa.Next) {
-// 	fmt.Println("c")
-// 	// next(nil)
-// })
-
-// app.Get("/json", func(err error, ctx *koa.Context, next koa.Next) {
-// 	ctx.SetHeader("Content-Type", "application/json")
-// 	data := make(map[string]interface{})
-// 	data["test"] = "test"
-// 	ret, _ := json.Marshal(data)
-// 	ctx.SetBody(ret)
-// })
-
-// app.Use(func(err error, ctx *koa.Context, next koa.Next) {
-// 	ctx.Status = 404
-// 	ctx.SetBody([]byte("Request not found"))
-// })
-// err := app.Run(8080)
-// if err != nil {
-// 	fmt.Println(err)
-// }
-// }
